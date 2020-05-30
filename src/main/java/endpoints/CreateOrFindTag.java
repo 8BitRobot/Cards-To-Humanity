09f763ed50a16849fe009377992258b4eaecbdd0
Oliver Lok Trevor <featherfeet5436@gmail.com>
2020-05-29 20:31:30 -0700

    /create_or_find_tag and /get_cards endpoints now work. I also wrote and tested the api* functions in api.js to access them.

---
+++ src/main/java/endpoints/CreateOrFindTag.java
@@ -0,0 +1,18 @@
+package endpoints;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import storage.DatabaseStorage;
+
+public class CreateOrFindTag implements Handler {
+    private DatabaseStorage databaseStorage;
+
+    public CreateOrFindTag(DatabaseStorage databaseStorage) {
+        this.databaseStorage = databaseStorage;
+    }
+
+    public void handle(Context ctx) {
+        String content = ctx.formParam("content", String.class).get();
+        ctx.contentType("text/plain");
+        ctx.result(String.valueOf(databaseStorage.createTagOrFindExisting(content)));
+    }
+}
